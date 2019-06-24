1b4f5d34635fbb92166001535c06d729f4340ee9
seevae <906435177@qq.com>
2019-06-24 21:46:01 +0800

    mybatis子查询,一级缓存,二级缓存

---
+++ mybatis_6/src/test/java/io/github/seevae/MyBatisTest.java
@@ -0,0 +1,16 @@
+package io.github.seevae;/*
+    name zhang;
+    */
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Before;
+
+public class MyBatisTest {
+    SqlSessionFactory sqlSessionFactory = null;
+
+    @Before
+    public void myInit(){
+
+    }
+
+}
