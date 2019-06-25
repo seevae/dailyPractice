b8521ab71916c4b42d90cac0a91373803f2922b6
seevae <906435177@qq.com>
2019-06-25 09:46:47 +0800

    延迟加载

--- mybatis_6/src/test/java/io/github/seevae/MyBatisTest.java
+++ mybatis_6/src/test/java/io/github/seevae/MyBatisTest.java
@@ -2,15 +2,47 @@ package io.github.seevae;/*
     name zhang;
     */
 
+import io.github.seevae.mapper.OrderMapper;
+import io.github.seevae.po.Orders;
+import io.github.seevae.po.User;
+import jdk.internal.util.xml.impl.Input;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class MyBatisTest {
+
     SqlSessionFactory sqlSessionFactory = null;
 
     @Before
-    public void myInit(){
+    public void myInit() throws IOException {
+        String file = "sqlMapConfig.xml";
+        InputStream  inputStream = Resources.getResourceAsStream(file);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Test
+    public void testQueryOrderUser() throws Exception {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        OrderMapper orderMapper =  sqlSession.getMapper(OrderMapper.class);
+        List<Orders> ordersList = orderMapper.queryOrderUser();
+        //延迟user
+        for(Orders orders:ordersList){
+            User user = orders.getUser();
+            System.out.println(user);
+        }
+
+        sqlSession.close();
 
     }
+
 
 }
