a91dca2bb77d1c3f859e6b19e078af2a3130a2e2
seevae <906435177@qq.com>
2019-06-29 22:32:46 +0800

    实现了多商品同时删除

--- ssmdome/src/main/java/io/github/seevae/mapper/ItemsCustomMapper.java
+++ ssmdome/src/main/java/io/github/seevae/mapper/ItemsCustomMapper.java
@@ -10,4 +10,6 @@ import java.util.List;
 
 public interface ItemsCustomMapper {
     public List<ItemsCustom> queryItemsByName(ItemsVO itemsVO) throws Exception;
+
+    public void deleteAll(Integer [] arr) throws Exception;
 }
