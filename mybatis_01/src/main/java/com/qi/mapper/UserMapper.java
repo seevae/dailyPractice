65daa51b657b60078909e616d2b634549f0a32b4
seevae <906435177@qq.com>
2019-08-05 16:12:12 +0800

    所有基本的CRUD操作完成

--- mybatis_01/src/main/java/com/qi/mapper/UserMapper.java
+++ mybatis_01/src/main/java/com/qi/mapper/UserMapper.java
@@ -4,8 +4,13 @@ package com.qi.mapper;/*
 
 import com.qi.po.User;
 
+import java.util.List;
+
 public interface UserMapper {
 
     public User findUserById(int id) throws Exception;
-
+    public void del(int id) throws Exception;
+    public void insertUser(User user) throws Exception;
+    public void updatebyid(User user) throws Exception;
+    public List<User> findAll() throws Exception;
 }
--- mybatis_01/src/test/java/com/qi/MybatisTest.java
+++ mybatis_01/src/test/java/com/qi/MybatisTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
 
@@ -32,6 +33,7 @@ public class MybatisTest {
             UserMapper userMaaper = (UserMapper) sqlSession.getMapper(UserMapper.class);
             User user = userMaaper.findUserById(10);
             System.out.println(user);
+            sqlSession.commit();
         }catch (Exception e){
             e.printStackTrace();
         }finally {
@@ -39,6 +41,79 @@ public class MybatisTest {
         }
     }
 
+    @Test
+    public void delById(){
+        SqlSession sqlSession  = sqlSessionFactory.openSession();
+        try{
+            //第一步,得到接口的代理对象
+            UserMapper userMaaper = sqlSession.getMapper(UserMapper.class);
+            userMaaper.del(33);
+            //一定要注意事务要进行提交,否则是没有变化的
+            sqlSession.commit();
+            System.out.println("删除完毕");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void insertUser(){
+        SqlSession sqlSession  = sqlSessionFactory.openSession();
+        try{
+            //第一步,得到接口的代理对象
+            UserMapper userMaaper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setUsername("小黄同学");
+            user.setSex(1);
+            user.setAddress("欧洲");
+            userMaaper.insertUser(user);
+            sqlSession.commit();
+            System.out.println("插入结束");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void updatebyid(){
+        SqlSession sqlSession  = sqlSessionFactory.openSession();
+        try{
+            //第一步,得到接口的代理对象
+            UserMapper userMaaper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setUsername("斯巴达");
+            user.setId(36);
+            userMaaper.updatebyid(user);
+            sqlSession.commit();
+            System.out.println("修改成功");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void findAll(){
+        SqlSession sqlSession  = sqlSessionFactory.openSession();
+        try{
+            //第一步,得到接口的代理对象
+            UserMapper userMaaper = sqlSession.getMapper(UserMapper.class);
+            List<User> list = userMaaper.findAll();
+            sqlSession.commit();
+            for(User u: list){
+                System.out.println(u);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+    }
 
 }
 
